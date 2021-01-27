e3039cb9ba6d6deb0bceb5b9dbaeda79c126df28
ali <alikasasbeh60@yahoo.com>
2021-01-27 15:25:28 +0200

    add JS page

---
+++ app.js
@@ -0,0 +1,17 @@
+//alert("hello");
+//document.write("hello in my website");
+//console.log("hello world");
+
+ //var studentInput =prompt("enter you'r grad");
+//if(studentInput>=50){
+  //  alert("succeed")
+//}else{
+  //  alert("faild");
+//}
+//confirm("change color");
+
+var person = prompt("Please enter your name", "Ali");
+
+if (person != null) {
+  alert("person");
+}
