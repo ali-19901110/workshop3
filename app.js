bd5ec9310850d01da5e11cfbfb571574005796db
ali <alikasasbeh60@yahoo.com>
2021-01-27 15:37:16 +0200

    add condition

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-//alert("hello");
-//document.write("hello in my website");
-//console.log("hello world");
+alert("hello");
+document.write("hello in my website");
+console.log("hello world");
 
- //var studentInput =prompt("enter you'r grad");
-//if(studentInput>=50){
-  //  alert("succeed")
-//}else{
-  //  alert("faild");
-//}
-//confirm("change color");
+ var studentInput =prompt("enter you'r grad");
+if(studentInput>=50){
+    alert("succeed")
+}else{
+    alert("faild");
+}
+confirm("change color");
 
 var person = prompt("Please enter your name", "Ali");
 
