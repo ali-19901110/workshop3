b22e919cf2491d94a2a763a211ec92520f03e5e2
ali <alikasasbeh60@yahoo.com>
2021-01-28 16:49:10 +0200

    add strech gal

--- app.js
+++ app.js
@@ -15,20 +15,22 @@
 //if (person != null) {
   //alert("you are welcome "+ person);
 //}
-var userorder = prompt("what would you like to order Pants or t-shirt","please enter here");
+var userorder = prompt("what would you like to order pants or t-shirt","please enter here");
 while(userorder !=="pants" && userorder!=="t-shirt"){
   userorder = prompt("please enter Pants or t-shirt");
 }
 var userInput = prompt("how many Pants or t-shirt do you wont?");
 var image ="";
 
-for(var i= 0 ;i < userInput ;i++){
+for(var i= 1 ;i <= userInput ;i++){
   if(userorder == "pants"){
     image = '<img src ="https://tierra.com/wp-content/uploads/2018/02/T1079210-dark-grey-1-800x973.jpg">';
     document.write(image);
+    document.write(i);
   }
   else if(userorder == "t-shirt"){
     image = '<img src ="https://imgprd19.hobbylobby.com/9/5f/26/95f264323ae49e65b2a53a909fcd7d9ee659f3c7/350Wx350H-422519-0320.jpg">';
     document.write(image);
+    document.write(i);
   }
 }
