bcf61d2d26c6cca11e975beff6a8e6ee99ce24d0
ali <alikasasbeh60@yahoo.com>
2021-02-06 20:24:05 +0200

    hello test

--- js/app.js
+++ js/app.js
@@ -15,28 +15,29 @@
 //if (person != null) {
   //alert("you are welcome "+ person);
 //}
-var userorder = prompt("what would you like to order pants or t-shirt","please enter order");
-function checkEnters(userorder){
-while(userorder !=="pants" && userorder!=="t-shirt"){
-  userorder = prompt("please enter Pants or t-shirt");
-}}  
+// var userorder = prompt("what would you like to order pants or t-shirt","please enter order");
+// function checkEnters(userorder){
+// while(userorder !=="pants" && userorder!=="t-shirt"){
+//   userorder = prompt("please enter Pants or t-shirt");
+// }}  
 
-checkEnters(userorder);
+// checkEnters(userorder);////here how can i reuse function
 
-var userInput = prompt("how many Pants or t-shirt do you wont?");
-function nc(userorder,userInput){
- for(var i= 1 ;i <= userInput ;i++){
-  var image ="";
-  if(userorder == "pants"){
-    image = '<img src ="https://tierra.com/wp-content/uploads/2018/02/T1079210-dark-grey-1-800x973.jpg">';
-    document.write(image);
-    document.write(i);
-  }
-  else if(userorder == "t-shirt"){
-    image = '<img src ="https://imgprd19.hobbylobby.com/9/5f/26/95f264323ae49e65b2a53a909fcd7d9ee659f3c7/350Wx350H-422519-0320.jpg">';
-    document.write(image);
-    document.write(i);
-  }
-}}
+// var userInput = prompt("how many Pants or t-shirt do you wont?");
+// function show(userorder,userInput){
+//  for(var i= 1 ;i <= userInput ;i++){
+//   var image ="";
+//   if(userorder == "pants"){
+//     image = '<img src ="https://tierra.com/wp-content/uploads/2018/02/T1079210-dark-grey-1-800x973.jpg">';
+//     document.write(image);
+//     document.write(i);
+//   }
+//   else if(userorder == "t-shirt"){
+//     image = '<img src ="https://imgprd19.hobbylobby.com/9/5f/26/95f264323ae49e65b2a53a909fcd7d9ee659f3c7/350Wx350H-422519-0320.jpg">';
+//     document.write(image);
+//     document.write(i);
+//   }
+// }}
 
-nc(userorder,userInput);
+// show(userorder,userInput); //here how can i reuse function
+for (var i = 4; i < 24; i = i + 2) {Console.WriteLine(i);}
